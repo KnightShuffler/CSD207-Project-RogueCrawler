5644692d756680bc9debf93b1046e62ed52c6a2e
KnightShuffler <knightshuffler@gmail.com>
2016-11-24 21:46:58 +0530

    Added Base Entity classes

---
+++ src/game/Enemy.java
@@ -0,0 +1,14 @@
+package game;
+
+import engine.ColorRGBA8;
+
+public abstract class Enemy extends Entity {
+	
+	public Enemy(int h, float x, float y, int width, int height, float speed, float shotSpeed, float fireRate,
+			int damage, int texture, ColorRGBA8 color) {
+		
+		super(h, x, y, width, height, speed, shotSpeed, fireRate, damage, texture, color);
+	}
+	
+	public abstract void update(Player p);
+}
