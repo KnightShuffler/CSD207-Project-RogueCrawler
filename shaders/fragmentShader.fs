838ce1ba3c5d020ffc76ef2bcdec80bb58b6895a
KnightShuffler <knightshuffler@gmail.com>
2016-11-30 19:12:55 +0530

    Allowed Color Multiplying in the shader

--- shaders/fragmentShader.fs
+++ shaders/fragmentShader.fs
@@ -10,10 +10,5 @@ in vec2 fragmentUV;
 out vec4 color;
 
 void main() {
-	/*vec4 col = vec4(clamp(fragmentColor.r * sin(2 * time + fragmentPosition.x), 0f, 1f),
-					clamp(fragmentColor.g * cos(5 *time + fragmentPosition.y), 0f, 1f),
-					clamp(fragmentColor.b * abs(tan(time)), 0f, 1f),
-					1f
-					);*/ 
-	color = texture2D(mySampler, fragmentUV);
+	color = texture2D(mySampler, fragmentUV) * fragmentColor;
 }
