02efa23f2e794b32c54cc6ff1c3d8a18b5743d65
Harsh <Harsh@Harsh-ENVY15>
2016-12-04 01:08:26 +0530

    Modified timer class to implement timestep. (for implementing physics)

--- src/engine/Timer.java
+++ src/engine/Timer.java
@@ -3,36 +3,35 @@ package engine;
 public class Timer {
 	// the framerate that the game should be running at in frames per second,
 	// the game can not run faster than this
-	private double targetFrameRate;
+	final public float targetFrameRate;
 	// the time taken for one optimal frame to execute
-	private double frameCap;
+	final public float frameCap;
 
 	// The time at start of the previous frame
-	private double time;
+	private float time;
 	// The time at the start of this frame
-	private double time2;
+	private float time2;
 	// The difference between time and time2
-	private double deltaTime;
-
-	// The amount of unprocessed time between frames
-	private double unprocessed;
-
-	private boolean shouldRender;
-
-	// Debugging members
-
+	private float timeDifference;
 	// The time taken since the last time the framerate was displayed
-	private double frameTime;
+	private float frameTime;
 	// The number of frames that have occured until now
 	private int frames;
 
-	public static double getTime() {
-		return (double) System.nanoTime() / (double) 1000000000L;
+	// The amount of unprocessed time between frames
+	private float unprocessed;
+
+	// Whether the frame should be rendered or not
+	private boolean shouldRender;
+
+	//Returns the system time in milliseconds
+	public static float getTime() {
+		return (float) System.nanoTime() / (float) 1000000000L;
 	}
 
-	public Timer(double target) {
+	public Timer(float target) {
 		targetFrameRate = target;
-		frameCap = 1.0 / targetFrameRate;
+		frameCap = 1f / targetFrameRate;
 		frameTime = 0;
 		frames = 0;
 
@@ -40,39 +39,77 @@ public class Timer {
 		unprocessed = 0;
 	}
 
-	public void beginFrame() {
-		time2 = Timer.getTime();
-		deltaTime = time2 - time;
-		unprocessed += deltaTime;
-		frameTime += deltaTime;
-		time = time2;
-		shouldRender = false;
-	}
+//	public void beginFrame() {
+//		time2 = Timer.getTime();
+//		timeDifference = time2 - time;
+//		unprocessed += timeDifference;
+//		frameTime += timeDifference;
+//		time = time2;
+//		
+//		shouldRender = false;
+//	}
 
-	public boolean shouldProcess() {
-		if (unprocessed >= frameCap) {
-			unprocessed -= frameCap;
-			shouldRender = true;
-			frames++;
-			return true;
-		} else {
-			return false;
-		}
-	}
+//	public boolean shouldProcess() {
+//		if (unprocessed >= frameCap) {
+//			unprocessed -= frameCap;
+//			shouldRender = true;
+//			frames++;
+//			return true;
+//		} else {
+//			return false;
+//		}
+//	}
 
-	public boolean shouldRender() {
-		return shouldRender;
-	}
+//	public boolean shouldRender() {
+//		return shouldRender;
+//	}
 
-	public void setTargetFrameRate(double fps) {
-		targetFrameRate = fps;
+//	public void displayFrameRate() {
+//		if (frameTime >= 1.0) {
+//			frameTime = 0;
+//			System.out.println("FPS: " + frames);
+//			frames = 0;
+//		}
+//	}
+	
+	private float frameStartTime;
+	private float frameEndTime;
+	private float frameDuration;
+	
+	private float prevTicks;
+	private float newTicks;
+	private float fTime;
+	
+	private float totalDeltaTime;
+	
+	public void init() {
+		prevTicks = getTime();
 	}
-
-	public void displayFrameRate() {
-		if (frameTime >= 1.0) {
-			frameTime = 0;
-			System.out.println("FPS: " + frames);
-			frames = 0;
+	
+	//calculates delta time
+	public void updateTimeStep() {
+		newTicks = getTime();
+		fTime = newTicks - prevTicks;
+		prevTicks = newTicks;
+		
+		totalDeltaTime = fTime / frameCap;
+//		totalDeltaTime = fTime / (1f / 60f);
+	}
+	
+	public float getTotalDeltaTime() {
+		return totalDeltaTime;
+	}
+	
+	public void startFrame() {
+		frameStartTime = getTime();
+	}
+	
+	public void endFrame() throws InterruptedException {
+		frameEndTime = getTime();
+		frameDuration = frameEndTime - frameStartTime;
+		
+		if (frameDuration < frameCap) {
+			Thread.sleep((long)(frameCap * 1000) - (long)(frameDuration * 1000f));
 		}
 	}
 }
